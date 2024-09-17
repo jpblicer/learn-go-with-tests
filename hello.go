e22b174c0b29be3021aa2c6fd56dec4cf89a91f2
James Young <jpblicer@gmail.com>
2024-09-17 20:43:45 +0900

    test for spanish, and return different greeting Hola if true

--- hello.go
+++ hello.go
@@ -9,6 +9,11 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
 	return englishHelloPrefix + name
 }
 
