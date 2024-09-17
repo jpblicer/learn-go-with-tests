5c2b03518ae3a942aefacb5f07ffa07e37e6e700
James Young <jpblicer@gmail.com>
2024-09-17 20:35:32 +0900

    refactor test_hello

--- hello_test.go
+++ hello_test.go
@@ -6,17 +6,19 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func (t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, want, got)
 	})
 	t.Run("say 'Hello, World' when empty string is is supplied", func (t *testing.T) {
 		got := Hello("")
 		want:= "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, want, got)
 	})
+}
+
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
