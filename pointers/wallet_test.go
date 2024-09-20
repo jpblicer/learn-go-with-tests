bdb94346fa5003769f5291eccc31c080d4a9c18d
James Young <jpblicer@gmail.com>
2024-09-20 21:57:49 +0900

    extend type int with Bitcoin type

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -7,13 +7,13 @@ import (
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
 
 	fmt.Printf("Address of the balance in test is %p \n", &wallet.balance)
 
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
