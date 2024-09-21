01249fc0e5a6a325f84feb9d89a3b75dd1d4cec8
James Young <jpblicer@gmail.com>
2024-09-21 13:45:54 +0900

    write test in situation there is no word in dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,12 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is a test"}
 
-	got := dictionary.Search("test")
-	want := "this is a test"
+	t.Run("known word", func(t *testing.T){
+		got := dictionary.Search("test")
+		want := "this is a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
 
+	t.Run("unknown word", func (t *testing.T)  {
+		_, err := dictionary.Search("unknown")
+		want := "could not find word you are looking for"
 
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
