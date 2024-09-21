50b12c7480b4cc8e655b007ac60ded43b70c53b4
James Young <jpblicer@gmail.com>
2024-09-21 14:32:23 +0900

    update add function to account for existing words

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,8 +4,11 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound =  errors.New("could not find word you are looking for")
 
+var (
+	ErrWordExists = errors.New("cannot add word as exists already")
+	ErrNotFound =  errors.New("could not find word you are looking for")
+)
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
@@ -16,6 +19,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
