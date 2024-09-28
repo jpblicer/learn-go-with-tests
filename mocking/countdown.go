9161d52ce71f3446d7f71b7104f7a828e365941a
James Young <jpblicer@gmail.com>
2024-09-28 21:35:27 +0900

    add second delay for countdown

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -16,6 +17,7 @@ func main () {
 func Countdown (out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Fprint(out, finalWord)
