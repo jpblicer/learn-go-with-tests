de4740f2ae1bf9f67561d7de6c12270ce4715610
James Young <jpblicer@gmail.com>
2024-10-01 16:48:29 +0900

    write Counter test

---
+++ sync/counter_test.go
@@ -0,0 +1,16 @@
+package sync
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	t.Run("3 increments on counter stays at 3", func(t *testing.T){
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		if counter.Value != 3 {
+			t.Errorf("got %d want %d", counter.Value(), 3)
+		}
+	})
+}
