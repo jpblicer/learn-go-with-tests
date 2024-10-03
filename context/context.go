fa13c4b8540e3c68945d6e45939890f82a40fc15
James Young <jpblicer@gmail.com>
2024-10-03 16:54:53 +0900

    Server now uses context of fetch to be done or cancel

--- context/context.go
+++ context/context.go
@@ -10,10 +10,20 @@ type Store interface {
 	Cancel()
 }
 
-func Server (store Store) http.HandlerFunc {
+func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
-
