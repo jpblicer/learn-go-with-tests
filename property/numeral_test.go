ff4ad53f6a64945e84005111b6886b5c00f649b7
James Young <jpblicer@gmail.com>
2024-10-03 20:57:58 +0900

    refactor test struct to just take arabic and roman. Increase tests

--- property/numeral_test.go
+++ property/numeral_test.go
@@ -1,35 +1,50 @@
 package property
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic int
+		Roman  string
 	}{
-		{"1 to I", 1, "I"},
-		{"2 to II", 2, "II"},
-		{"3 to III", 3, "III"},
-		{"4 to IV", 4, "IV"},
-		{"5 to V", 5, "V"},
-		{"6 to VI", 6, "VI"},
-		{"7 to VII", 7, "VII"},
-		{"8 to VIII", 8, "VIII"},
-		{"9 to IX", 9, "IX"},
-		{"10 to X", 10, "X"},
-		{"40 to XL", 40, "XL"},
-		{"47 to XLVII", 47, "XLVII"},
-		{"49 to XLIX", 49, "XLIX"},
-		{"50 to L", 50, "L"},
+		{Arabic: 1, Roman: "I"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 6, Roman: "VI"},
+		{Arabic: 7, Roman: "VII"},
+		{Arabic: 8, Roman: "VIII"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 10, Roman: "X"},
+		{Arabic: 14, Roman: "XIV"},
+		{Arabic: 18, Roman: "XVIII"},
+		{Arabic: 20, Roman: "XX"},
+		{Arabic: 39, Roman: "XXXIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 47, Roman: "XLVII"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 50, Roman: "L"},
+		{Arabic: 100, Roman: "C"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 400, Roman: "CD"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 1006, Roman: "MVI"},
+		{Arabic: 798, Roman: "DCCXCVIII"},
 	}
-
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-
-			if got != test.Want {
-				t.Errorf("got %q want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
 			}
 		})
 	}
