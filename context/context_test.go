702be478b8947805ee9c0f4963de9498b17d1142
James Young <jpblicer@gmail.com>
2024-10-03 16:50:31 +0900

    refactor test and check for store cancelled to be true

--- context/context_test.go
+++ context/context_test.go
@@ -23,8 +23,10 @@ func (s *SpyStore) Cancel() {
 }
 
 func TestServer(t *testing.T) {
+	t.Run("returns data from store", func(t *testing.T) {
 	data := "hello world"
-	svr := Server(&SpyStore{response: data})
+	store := &SpyStore{response: data}
+	svr := Server(store)
 
 	request := httptest.NewRequest(http.MethodGet, "/", nil)
 	response := httptest.NewRecorder()
@@ -34,6 +36,13 @@ func TestServer(t *testing.T) {
 	if response.Body.String() != data {
 		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
 	}
+
+	if store.cancelled {
+		t.Error("it should have cancelled store")
+}
+
+})
+
 
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
 		data := "hello, world"
