dadb5a011c157a4c7d66a24fe4946774b5a9e49a
James Young <jpblicer@gmail.com>
2024-10-03 18:13:51 +0900

    refactor test to table test

--- property/numeral_test.go
+++ property/numeral_test.go
@@ -3,22 +3,22 @@ package property
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 to I", 1, "I"},
+		{"2 to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
 
-	t.Run("2 to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
-
+			if got != test.Want {
+				t.Errorf("got %q want %q", got, test.Want)
+			}
+		})
+	}
 }
