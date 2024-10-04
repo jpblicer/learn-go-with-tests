75160953df81e45e35c69f46370bf4c1f5b07324
James Young <jpblicer@gmail.com>
2024-10-04 18:22:56 +0900

    fix error in test math, and write math for secondhand

--- clock/clockface/clockface_acceptance_test.go
+++ clock/clockface/clockface_acceptance_test.go
@@ -18,7 +18,7 @@ func TestSecondHandAtMidnight(t *testing.T) {
 }
 
 func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
 
 	want := clockface.Point{X: 150, Y: 150 + 90}
 	got := clockface.SecondHand(tm)
